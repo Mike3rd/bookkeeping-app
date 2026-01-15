1d00be1d52dda3347efdd5d8dababd9cc148b915
MikeTurko <mturko@outlook.com>
2026-01-15 15:39:17 -0500

    fix

--- app/admin/components/AdminHeader.tsx
+++ app/admin/components/AdminHeader.tsx
@@ -174,7 +174,7 @@ export default function AdminHeader() {
                             className="flex items-center gap-2 px-4 py-2 rounded-lg hover:bg-red-600 bg-red-500 transition-colors ml-2"
                         >
                             <LogOut className="w-5 h-5" />
-                            <span>Logout2</span>
+                            <span>Logout</span>
                         </button>
                     </nav>
 
